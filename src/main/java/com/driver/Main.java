d22132a4c278178dcdae5e47f4a8a68be3cddaaf
abhinavsurs <abhinavsuresh.11@gmail.com>
2022-10-06 13:57:27 +0530

    Done the tasks in the read me file

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,26 @@
 package com.driver;
 
 public class Main {
+    public static class Product{
+        public int product(int x,int y){
+             return -1;
+        }
+        public int product(int x,int y,int z){
+            return -1;
+
+        }
+        public double product(double x,double y){
+            return -1;
+        }
+
+    }
+
+    public static void main(String[] args) {
+        Product p =new Product();
+        p.product(7,8);
+        p.product(3,4,5);
+        p.product(1.3,6.7);
+    }
+
 
 }
